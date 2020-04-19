3c7a66bd1914dcc4d606f578ac612459dc1aa27d
Bruno Almeida <brunosalmeida@outlook.com>
2020-04-19 20:37:08 +0100

    DiagonalDifference Challenge

---
+++ HackerRank/DiagonalDifference.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank
+{
+    public class DiagonalDifference
+    {
+        public static int Difference(List<List<int>> arrays)
+        {
+            var sumResultRightToLeft = SumRightToLeftDiagonal(arrays);
+            var sumResultLeftToRight = SumLeftToRightDiagonal(arrays);
+
+            return sumResultRightToLeft - sumResultLeftToRight;
+        }
+
+        public static int SumLeftToRightDiagonal(List<List<int>> arrays)
+        {
+            int counter = 1;
+            int difference = 0;
+          
+            for (int startIndex = 0; startIndex < arrays.Count; startIndex++)
+            {
+                var current = arrays[startIndex];
+                var nextIndex = startIndex + 1;
+
+                if (nextIndex < arrays.Count)
+                {
+                    var next = arrays[nextIndex];
+
+                    if (next.Count < current.Count)
+                    {
+                        counter = next.Count;
+                        difference = 0;
+
+                        break;
+                    }
+                }
+
+                difference += current[current.Count - counter];
+                counter++;
+            }
+
+            return difference;
+        }
+
+        public static int SumRightToLeftDiagonal(List<List<int>> arrays)
+        {
+            int index = 0;
+            int difference = 0;
+
+            for (int startIndex = 0; startIndex < arrays.Count; startIndex++)
+            {
+                var current = arrays[startIndex];
+                var nextIndex = startIndex + 1;
+
+                if (nextIndex < arrays.Count)
+                {
+                    var next = arrays[nextIndex];
+
+                    if (next.Count < current.Count)
+                    {
+                        index = 0;
+                        difference = 0;
+
+                        break;
+                    }
+                }
+
+                difference += current[index];
+                index++;
+            }
+
+            return difference;
+        }
+    }
+}
---
+++ HackerRankTest/DiagonaDifferenceTest.cs
@@ -0,0 +1,64 @@
+﻿using HackerRank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace HackerRankTest
+{
+    [TestClass]
+    public class DiagonaDifferenceTest
+    {
+        [TestMethod]
+        public void SumRightToLeftTest()
+        {
+            var arrays = new List<List<int>>();
+
+            var array0 = new List<int>() { 1, 3, 5 };
+            var array1 = new List<int>() { 7, 9, 11 };
+            var array2 = new List<int>() { 12, 15, 17 };
+
+            arrays.Add(array0);
+            arrays.Add(array1);
+            arrays.Add(array2);
+
+            var result = DiagonalDifference.SumRightToLeftDiagonal(arrays);
+
+            Assert.AreEqual((1 + 9 + 17), result);
+        }
+
+        [TestMethod]
+        public void SumLeftToRightTest()
+        {
+            var arrays = new List<List<int>>();
+
+            var array0 = new List<int>() { 1, 3, 5 };
+            var array1 = new List<int>() { 7, 9, 11 };
+            var array2 = new List<int>() { 12, 15, 17 };
+
+            arrays.Add(array0);
+            arrays.Add(array1);
+            arrays.Add(array2);
+
+            var result = DiagonalDifference.SumLeftToRightDiagonal(arrays);
+
+            Assert.AreEqual((5 + 9 + 12), result);
+        }
+
+        [TestMethod]
+        public void DifferenceTest()
+        {
+            var arrays = new List<List<int>>();
+
+            var array0 = new List<int>() { 1, 3, 5 };
+            var array1 = new List<int>() { 7, 9, 11 };
+            var array2 = new List<int>() { 12, 15, 17 };
+
+            arrays.Add(array0);
+            arrays.Add(array1);
+            arrays.Add(array2);
+
+            var result = DiagonalDifference.Difference(arrays);
+
+            Assert.AreEqual((1 + 9 + 17) - (5 + 9 + 12), result);
+        }
+    }
+}
