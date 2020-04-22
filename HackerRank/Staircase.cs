cf8530ae09c3c4c7c07bad7c8e4a5f8bb475d9cb
Bruno Almeida <brunosalmeida@outlook.com>
2020-04-22 22:04:26 +0100

    Staircase challenge

---
+++ HackerRank/Staircase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank
+{
+    public class Staircase
+    {
+        public static string[] Stair(int number)
+        {
+            const char symbol = '#';
+
+            var stair = new string[number];
+
+            for (int index = 0; index < number; index++)
+            {
+                int counter = index + 1;
+                int length = number - counter;
+
+                string symbols = new string(symbol, counter); 
+
+                string stairs = new String(' ', length).Insert(length, symbols);
+                
+                stair[index] = stairs;
+
+                Console.WriteLine(stairs);
+            }
+
+            return stair;
+        }
+    }
+}
---
+++ HackerRankTest/StaircaseTest.cs
@@ -0,0 +1,22 @@
+﻿using HackerRank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankTest
+{
+    [TestClass]
+    public class StaircaseTest
+    {
+        [TestMethod]
+        public void StaircasePrint()
+        {
+            var result = Staircase.Stair(6);
+
+            Assert.AreEqual("     #", result[0]);
+            Assert.AreEqual("    ##", result[1]);
+            Assert.AreEqual("   ###", result[2]);
+            Assert.AreEqual("  ####", result[3]);
+            Assert.AreEqual(" #####", result[4]);
+            Assert.AreEqual("######", result[5]);
+        }
+    }
+}
