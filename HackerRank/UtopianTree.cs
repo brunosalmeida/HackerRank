01e8ee49bdbac08f3f9873d15e190931f4928e34
Bruno Almeida <brunosalmeida@outlook.com>
2020-04-26 17:51:33 +0100

    UtopianTree Challenge

---
+++ HackerRank/UtopianTree.cs
@@ -0,0 +1,24 @@
+﻿namespace HackerRank
+{
+    public class UtopianTree
+    {
+        public static int CalcHeight(int cycles)
+        {
+            var height = 1;
+            var i = 1;
+
+            while (i <= cycles)
+            {
+                if (i % 2 == 0)
+                    height += 1;
+                else
+                    height *= 2;
+
+                i++;    
+            }
+
+
+            return height;
+        }
+    }
+}
---
+++ HackerRankTest/UtopianTreeTest.cs
@@ -0,0 +1,44 @@
+﻿using HackerRank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRankTest
+{
+    [TestClass]
+    public class UtopianTreeTest
+    {
+        [TestMethod]
+        public void Calc0CycleTest()
+        {
+            var result = UtopianTree.CalcHeight(0);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Calc1CycleTest()
+        {
+            var result = UtopianTree.CalcHeight(1);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Calc4CyclesTest()
+        {
+            var result = UtopianTree.CalcHeight(4);
+
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void Calc5CyclesTest()
+        {
+            var result = UtopianTree.CalcHeight(5);
+
+            Assert.AreEqual(14, result);
+        }
+    }
+}
