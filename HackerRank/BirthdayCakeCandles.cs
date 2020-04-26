fdd05d0556ff06477fff4830d3acced09c5e3deb
Bruno Almeida <brunosalmeida@outlook.com>
2020-04-26 15:03:22 +0100

    Birthday Cake Candles Challenge

---
+++ HackerRank/BirthdayCakeCandles.cs
@@ -0,0 +1,28 @@
+﻿namespace HackerRank
+{
+    public class BirthdayCakeCandles
+    {
+        public static int Calc(int[] array)
+        {
+            var counter = 0;
+
+            var max = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                var tmp = array[i];
+
+                if (tmp > max)
+                    max = tmp;
+            }
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (max == array[j])
+                    counter++;
+            }           
+
+            return counter;
+        }
+    }
+}
---
+++ HackerRankTest/BirthdayCakeCandlesTest.cs
@@ -0,0 +1,19 @@
+﻿using HackerRank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace HackerRankTest
+{
+    [TestClass]
+    public class BirthdayCakeCandlesTest
+    {
+        [TestMethod]
+        public void Calc()
+        {
+            var array = new int[] { 3, 2, 1, 3 };
+            var result = BirthdayCakeCandles.Calc(array);
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
