905a562dee0836efe7b03c40d768a83eaca42172
Bruno Almeida <brunosalmeida@outlook.com>
2020-04-26 14:07:03 +0100

    MiniMaxSum Challenge

---
+++ HackerRank/MiniMaxSum.cs
@@ -0,0 +1,36 @@
+﻿namespace HackerRank
+{
+    public class MiniMaxSum
+    {
+        public static string Sum(int[] array)
+        {
+            var max = 0;
+            var min = 0;
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                var tmp = 0;
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (index != i)
+                    {
+                        tmp += array[i];
+                    }
+                }
+
+                if (max == 0 || tmp > max)
+                {
+                    max = tmp;
+                }
+
+                if (min == 0 || tmp < min)
+                {
+                    min = tmp;
+                }
+            }
+
+            return $"{min} {max}";
+        }
+    }
+}
---
+++ HackerRankTest/MiniMaxSumTest.cs
@@ -0,0 +1,20 @@
+﻿using HackerRank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace HackerRankTest
+{
+    [TestClass]
+    public class MiniMaxSumTest
+    {
+        [TestMethod]
+        public void SumTest()
+        {
+            var array = new int[] { 1, 2, 3, 4, 5 };
+
+            var result = MiniMaxSum.Sum(array);
+
+            Assert.AreEqual("10 14", result);
+        }
+    }
+}
