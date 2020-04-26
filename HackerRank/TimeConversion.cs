e58649fa0b803279614da97ca23eed5b9c5d3a75
Bruno Almeida <brunosalmeida@outlook.com>
2020-04-26 16:49:50 +0100

    TimeConversion Challenge

---
+++ HackerRank/TimeConversion.cs
@@ -0,0 +1,25 @@
+﻿namespace HackerRank
+{
+    public class TimeConversion
+    {
+        public static string Convert(string date)
+        {
+            var hour = int.Parse(date.Split(':')[0]);
+            var minute = date.Split(':')[1];
+            var seconds = date.Split(':')[2].Substring(0,2);
+
+
+            var type = date.Substring(date.Length - 2);
+
+            if (hour != 12 && type.ToUpper() == "PM")
+                hour += 12;
+
+            if (hour == 12 && type.ToUpper() == "AM")
+                hour = 0;
+            
+            var convertedDate = hour < 10? $"0{hour}:{minute}:{seconds}" : $"{hour}:{minute}:{seconds}";
+
+            return convertedDate;
+        }
+    }
+}
---
+++ HackerRankTest/TimeConversionTest.cs
@@ -0,0 +1,38 @@
+﻿using HackerRank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace HackerRankTest
+{
+    [TestClass]
+    public class TimeConversionTest
+    {
+        [TestMethod]
+        public void ConvertPMTest()
+        {
+            var date = "07:05:45PM";
+            var result = TimeConversion.Convert(date);
+
+            Assert.AreEqual("19:05:45", result);
+        }
+
+        [TestMethod]
+        public void ConvertAMTest()
+        {
+            var date = "07:05:45AM";
+            var result = TimeConversion.Convert(date);
+
+            Assert.AreEqual("07:05:45", result);
+        }
+
+        [TestMethod]
+        public void Convert00AMTest()
+        {
+            var date = "00:00:00AM";
+            var result = TimeConversion.Convert(date);
+
+            Assert.AreEqual("00:00:00", result);
+        }
+
+    }
+}
